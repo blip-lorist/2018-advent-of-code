8897c6199c80db7c9ef88fb6214eee0bc086582d
Loraine <loraine@lorainekv.com>
2018-12-07 00:57:45 -0800

    Misunderstood the instructions

--- 07/part_one.py
+++ 07/part_one.py
@@ -34,53 +34,82 @@ for step in steps:
         prereq_lookup[next_step] = set(prereq_step)
 
 
-for step in steps:
-    step = step.split(" ")
-    prereq_step = step[1]
-    next_step = step[7]
-    import pdb; pdb.set_trace()
+# Incorrect route - I was relying on the order of the steps
+# as they came in
+#for step in steps:
+#    step = step.split(" ")
+#    prereq_step = step[1]
+#    next_step = step[7]
+#    import pdb; pdb.set_trace()
+#
+#    # start the tree
+#    if len(tree) == 0:
+#        prereq = prereq_lookup.get(prereq_step, None)
+#        if meets_requirements(prereq, tree):
+#            tree.append(prereq_step)
+#            available.append(next_step)
+#            continue
+#
+#
+#    if prereq_step not in tree:
+#        available.append(prereq_step)
+#
+#    if next_step not in tree:
+#        available.append(next_step)
+#
+#    available = OrderedSet(sorted(available))
+#    available_added = False
+#    for available_step in available:
+#        prereq = prereq_lookup.get(available_step, None)
+#        if meets_requirements(prereq, tree) and available_step not in tree:
+#            tree.append(available_step)
+#            import pdb; pdb.set_trace()
+#            available.discard(available_step)
+#            available_added = True
+#            break
+#
+#    if available_added:
+#        continue
+#
+#    if len(tree) == (len(prereq_lookup) + 1): # the tree is complete
+#        continue
+#    else:
+#        # no action can be taken this turn
+#        continue
 
-    # start the tree
-    if len(tree) == 0:
-        prereq = prereq_lookup.get(prereq_step, None)
-        if meets_requirements(prereq, tree):
-            tree.append(prereq_step)
-            available.append(next_step)
-            continue
+step = steps[0]
+step = step.split(" ")
+prereq_step = step[1]
+next_step = step[7]
 
-
-    if prereq_step not in tree:
-        available.append(prereq_step)
-
-    if next_step not in tree:
+# start the tree
+if len(tree) == 0:
+    prereq = prereq_lookup.get(prereq_step, None)
+    if meets_requirements(prereq, tree):
+        tree.append(prereq_step)
         available.append(next_step)
 
-    available = OrderedSet(sorted(available))
-    available_added = False
+
+# Correct route - use the alphabet first, check against prerequisites
+import string
+alphabet = list(string.ascii_uppercase)
+for letter in alphabet:
+
+    if letter not in tree:
+        available.append(letter)
+
+available = OrderedSet(sorted(available))
+while len(available) != 0:
     for available_step in available:
         prereq = prereq_lookup.get(available_step, None)
         if meets_requirements(prereq, tree) and available_step not in tree:
             tree.append(available_step)
-            import pdb; pdb.set_trace()
             available.discard(available_step)
-            available_added = True
             break
-
-    if available_added:
-        continue
-
-    if len(tree) == (len(prereq_lookup) + 1): # the tree is complete
-        continue
-    else:
-        # no action can be taken this turn
-        continue
 
 
 print("".join(tree))
 
 
-    # if bst is  empty, insert prereq, put next_step in holding
-    # if prereq is not in holding, disregard
-    #   # check next_step against holding, sort alphabetically, add to tree
 
 
