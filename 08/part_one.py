bd16ed40478db33ae444c9efbbaa9a5a866582cd
Loraine <loraine@lorainekv.com>
2018-12-07 22:45:59 -0800

    Works for test data

--- 08/part_one.py
+++ 08/part_one.py
@@ -1,4 +1,3 @@
-
 class Node:
     def __init__(self, children_count, metadata_count, metadata):
         self.children_count = children_count
@@ -8,24 +7,37 @@ class Node:
 def recursive_find_children(tree, children=[]):
     tree = [int(x) for x in tree]
     header = tree[0:2]
+    children_count, metadata_count = header
 
-    if len(tree) == 0:
+    min_size = len(header) + (children_count * len(header)) + metadata_count
+    if len(tree) == 0 or len(tree) < min_size:
         return children
 
-    children_count, metadata_count = header
-    import pdb; pdb.set_trace()
-
     if children_count > 0:
-        return recursive_find_children(tree[2:], children)
+        # Branch
+        branch_children = recursive_find_children(tree[2:],[])
+        # adds itself as a child
+        total_child_size = 0
+        for child in branch_children:
+            size = len(child.metadata) + 2
+            total_child_size += size
+
+        self_node_end_idx = 1 + (total_child_size) + metadata_count
+        self_metadata_start_idx = self_node_end_idx - metadata_count + 1
+        node = Node(children_count, metadata_count, tree[self_metadata_start_idx:self_node_end_idx + 1])
+        children.append(node)
+        children += branch_children
+        return children
     else:
+        # Leaf
         metadata_start_idx = 2
         if metadata_count == 0:
             metadata_idx_diff = 0
         else:
             metadata_idx_diff = (metadata_count - 1)
-        metadata_end_idx = metadata_start_idx + metadata_idx_diff
-        node = Node(children, metadata_count, tree[metadata_start_idx:metadata_end_idx + 1])
-        children.append(node)
+            metadata_end_idx = metadata_start_idx + metadata_idx_diff
+            node = Node(children, metadata_count, tree[metadata_start_idx:metadata_end_idx + 1])
+            children.append(node)
 
         tree_remainder = tree[(metadata_end_idx + 1):]
         if len(tree_remainder) > 0:
@@ -37,7 +49,12 @@ def recursive_find_children(tree, children=[]):
 def main():
     tree = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2".split(" ")
     children = recursive_find_children(tree)
-    import pdb; pdb.set_trace()
+    metadata_sum = 0
+    for child in children:
+        for metadatum in child.metadata:
+            metadata_sum += metadatum
+
+    print(metadata_sum)
 
 
 main()
