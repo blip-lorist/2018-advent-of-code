7fff921594b487b5501612101edde470b9033d9d
Loraine <loraine@lorainekv.com>
2018-12-11 18:49:47 -0800

    Part one answered

--- 11/part_one.py
+++ 11/part_one.py
@@ -1,6 +1,32 @@
+import operator
 # configurable
 # size of grid
 # serial number
+
+
+def find_highest_cell(grid):
+    cell_locations_and_score = {}
+    grid_wall = len(grid) - 1
+    for idx_row, row in enumerate(grid):
+        for idx_col, col in enumerate(row):
+            if (idx_row + 2 > grid_wall) or (idx_col + 2 > grid_wall):
+                continue
+
+            top_left_x = idx_col
+            top_left_y = idx_row
+            cell_score = 0
+            for y in range(top_left_y, top_left_y + 3):
+                for x in range(top_left_x, top_left_x + 3):
+                    cell_score += grid[y][x]
+
+            cell_key = (top_left_x, top_left_y)
+            cell_locations_and_score[cell_key] = cell_score
+
+    highest_score_coords = sorted(cell_locations_and_score.items(), key=operator.itemgetter(1))[-1][0]
+    print(highest_score_coords[0] + 1)
+    print(highest_score_coords[1] + 1)
+
+
 
 
 def generate_fuel_cell_grid(size_of_grid, serial_number):
@@ -16,6 +42,7 @@ def generate_fuel_cell_grid(size_of_grid, serial_number):
         grid.append(row)
 
     return grid
+
 
 def get_power_level(x, y, serial_number):
     rack_id = x + 10
@@ -37,4 +64,5 @@ def get_power_level(x, y, serial_number):
 #print(get_power_level(217,196,39))
 #print(get_power_level(101,153,71))
 
-grid = generate_fuel_cell_grid(48, 18)
+grid = generate_fuel_cell_grid(300, 7857)
+find_highest_cell(grid)
